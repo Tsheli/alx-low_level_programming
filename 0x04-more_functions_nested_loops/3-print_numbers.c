73418a6ac183ad8f9dee585fc382e648c8caf822
Tsheli <beverlyseliane@gmail.com>
2022-10-22 09:47:11 +0000

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,10 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
  * print_numbers - Prints the numbers from 0-9.
  *
- * Return: The numbers since 0 up to 9
+ * Return;
  */
 void print_numbers(void)
 {
@@ -11,7 +12,7 @@ void print_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(num + '0');
+		_putchar((num % 10) + '0');
 	}
 
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
  * Definetion
  * and function prototypes
  */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
